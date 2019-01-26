74a461462f19896b6dce52d85a226d4da0f69ac1
Ben Wilson <ben.wilson1337@gmail.com>
2019-01-26 18:27:40 -0500

    Add proper zoom + pan

--- src/kanji-map.js
+++ src/kanji-map.js
@@ -2,15 +2,12 @@
 
 const Paper = require('paper');
 const Path = Paper.Path;
-
-const map = $('map');
-const canvas = $('canvas');
-canvas.style.left = map.offsetLeft; 
-canvas.style.top = map.offsetTop; 
+var canvas;
+var group;
+var zoomFactor = 2;
 
 let kanjiCoords = [];
 
-$('api-submit-button').addEventListener("click", onSubmitButtonClick);
 
 function $(id) {
 	return document.getElementById(id);
@@ -18,7 +15,20 @@ function $(id) {
 
 function setup() {
 	console.log('Setting up...');
+
+	// event listeners
+	$('api-submit-button').addEventListener("click", onSubmitButtonClick);
+
+	canvas = $('canvas');
 	Paper.setup(canvas);
+
+	var map = new Paper.Raster('map');
+	map.position = Paper.view.center;
+	map.fitBounds(Paper.view.bounds);
+	group = new Paper.Group(map);
+	// map.scale(0.5);
+	// map.rotate(45);
+
 	// var path = new Path();
 	// path.strokeColor = 'black';
 	// var start = new Paper.Point(100, 100);
@@ -28,22 +38,23 @@ function setup() {
 	console.log("Done drawing!");
 
 	// var path = new Path();
-	// var circlePath = new Path.Circle(new Paper.Point(50, 50), 25);
-	// circlePath.fillColor = 'red';
+	// pivot = new Path.Circle(Paper.view.center, 10);
+	// pivot.fillColor = 'green';
+	// group.addChild(pivot);
 
 	// path = new Path();
 	// var rect = new Path.Rectangle(20, 20, 40, 80);
 	// rect.fillColor = "rgba(255, 0, 0, 0.5)";
 
-	// Paper.onMouseMove = (event) => {
-	// 	circlePath.position = event.point;
-	// }
+	Paper.view.onKeyDown = onKeyDown;
+	Paper.view.onMouseEnter = onMouseEnter;
+	Paper.view.onMouseLeave = onMouseLeave;
+	Paper.view.onMouseMove = onMouseMove;
 
 	getUserKanji("f1513ed8-8f45-4fd6-9d45-1a2486cc65ba");
 }
 
-
-setup();
+window.onload = () => { setup(); }
 
 
 // ===========================================
@@ -57,9 +68,12 @@ function onSubmitButtonClick() {
 function getUserKanji(token) {
 	// const num = 3002;
 	// for (var i = 1; i <= num; i++)
-	// 	drawSquare(indexToCoord(i));
+	// 	kanjiCoords.push(indexToCoord(i));
+	// onUserDataSuccess();
 
-	// get list of inds from API
+
+
+	//get list of inds from API
 	( async () => {
 	  const response = await fetch('http://localhost:8081/api/ids', {
 	  	headers: {
@@ -92,6 +106,10 @@ function onUserDataFailure(error) {
 }
 
 function indexToCoord(i) {
+	if (i <= 0)  {
+		console.log('Got one of the 8 undefined kanji.');
+		return { x: -1, y: -1 };
+	}
 	if (i === 3002) return { x: 1, y: 50 }; // literally the only one out of place
 	else {
 		i--;
@@ -105,9 +123,12 @@ function indexToCoord(i) {
 // draw square for each point
 function drawKanjiSquares(alpha) {
 	// ctx.clearRect(0, 0, canvas.width, canvas.height);
+	var squares = [];
 	kanjiCoords.forEach(square => {
-		drawSquare(square);
+		// this 'if' will catch the 8 undefined kanji
+		if (square.x !== -1) squares.push(drawSquare(square));
 	});
+	group.addChildren(squares);
 }
 
 function drawSquare(square, alpha) {
@@ -116,7 +137,7 @@ function drawSquare(square, alpha) {
 
 	let w = 800; // image width
 	let realx = square.x + 2; // offset
-	let realy = square.y + 1; // offset;
+	let realy = square.y + 3; // offset;
 	realx += Math.floor(square.x / 10);
 	realy += Math.floor(square.y / 10)
 	let checkw = 11.95;
@@ -130,6 +151,45 @@ function drawSquare(square, alpha) {
 	var rect = new Path.Rectangle(realx, realy, checkw, checkh);
 	rect.fillColor = "rgba(255, 0, 0, " + alpha + ")";
 	// rect.stokeColor = "rgba(128,128,128,.1)";
+	return rect;
+}
+
+function onKeyDown(event) {
+	const zoomInc = .2;
+	if (event.key === 'w') {
+		zoomFactor += zoomInc;
+		group.scale(1 + zoomInc);
+	}	else if (event.key === 's') {
+		zoomFactor -= zoomInc;
+		group.scale(1 + (-1 * zoomInc));
+	}
+	console.log(zoomFactor);
+}
+
+function onMouseEnter(event) {
+	console.log('\\  Mouse enter!');
+	group.scale(zoomFactor);
+}
+
+function onMouseLeave(event) {
+	console.log('/  Mouse leave!');
+	group.fitBounds(Paper.view.bounds);
+	group.position = Paper.view.center;
+}
+
+function onMouseMove(event) {
+	// transform mouse point
+	let x = event.point.x / Paper.view.viewSize.width;
+	let y = event.point.y / Paper.view.viewSize.height;
+	x = 1 - (x + .25);
+	y = 1 - (y + .25);
+	x *= group.bounds.width;
+	y *= group.bounds.height;
+
+	// console.log(x + ', ' + y);
+	let point = new Paper.Point(x, y);
+	// console.log (' |  ' + point);
+	group.position = point;
 }
 
 var slider = $("myRange");
